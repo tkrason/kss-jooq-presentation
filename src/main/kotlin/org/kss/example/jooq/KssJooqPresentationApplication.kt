2e7ff44a401f3e5b988cc00cd703c9d442bb2778
Tomas Krason <tomas.krason@cleevio.com>
2025-01-04 20:59:10 +0100

    Initial commit

---
+++ src/main/kotlin/org/kss/example/jooq/KssJooqPresentationApplication.kt
@@ -0,0 +1,11 @@
+package org.kss.example.jooq
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KssJooqPresentationApplication
+
+fun main(args: Array<String>) {
+	runApplication<KssJooqPresentationApplication>(*args)
+}
---
+++ src/test/kotlin/org/kss/example/jooq/KssJooqPresentationApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.kss.example.jooq
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KssJooqPresentationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
